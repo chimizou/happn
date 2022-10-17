cec03416b8ca44a9e48ad48b51af474eb08e451e
chimizou <hamza.benbouzid@gmail.com>
2022-10-17 16:28:45 +0200

    adding service getEmployeeById, addEmployee, updateEmployee, deleteEmployee

--- employeemanager/src/main/java/com/chimizou/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/com/chimizou/employeemanager/service/EmployeeService.java
@@ -1,28 +1,44 @@
 package com.chimizou.employeemanager.service;
 
+import com.chimizou.employeemanager.exception.UserNotFoundException;
 import com.chimizou.employeemanager.model.Employee;
 import com.chimizou.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
 
-    private EmployeeRepo employeeRepo;
-
+    @Autowired
     public EmployeeService(EmployeeRepo employeeRepo) {
         this.employeeRepo = employeeRepo;
     }
 
-    public List<Employee> getAllEmployees() {
-        List<Employee> listEmployees = employeeRepo.findAll();
-        return listEmployees;
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
     }
 
-    public void addEmployee(Employee employee) {
-        employee.setEmployeeCode(UUID.randomUUID().toString());
-        employeeRepo.save(employee);
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
